76b56c8a446352fa088a14368e8f865a3e058c11
Aviad Segev <aviads@checkpoint.com>
2024-07-22 17:23:32 +0300

    Updated package to our forked repositories

--- Package.swift
+++ Package.swift
@@ -17,9 +17,9 @@ let package = Package(
         .package(url: "https://github.com/apple/swift-log.git", from: "1.4.0"),
         .package(url: "https://github.com/apple/swift-metrics.git", "1.0.0"..<"3.0.0"),
         .package(url: "https://github.com/apple/swift-distributed-tracing.git", from: "1.0.1"),
-        .package(url: "https://github.com/apple/swift-nio.git", from: "2.58.0"),
-        .package(url: "https://github.com/swift-server/swift-service-lifecycle.git", from: "1.0.0-alpha.9"),
-        .package(url: "https://github.com/hummingbird-project/hummingbird-core.git", from: "1.6.1"),
+        .package(url: "https://github.com/chkp-aviads/swift-nio.git", branch: "main"),
+        .package(url: "https://github.com/chkp-aviads/swift-service-lifecycle.git", branch: "1.0.0-alpha"),
+        .package(url: "https://github.com/chkp-aviads/hummingbird-core.git", branch: "main"),
     ],
     targets: [
         .target(name: "Hummingbird", dependencies: [
